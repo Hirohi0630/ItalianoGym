211779a278b78cf9c7c58ea84e506b14b7b2971e
Hirohi0630 <katayama.hiroko@gmail.com>
2026-07-12 14:01:13 +0900

    記録を入力する

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 function saluta() {
-    alert("Ciao! 🇮🇹");
+
+    let peso = document.getElementById("peso").value;
+    let ripetizioni = document.getElementById("ripetizioni").value;
+
+    alert(peso + "kg を " + ripetizioni + "回記録しました！💪");
+
 }
